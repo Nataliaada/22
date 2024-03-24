044ee125de5f591e54cdecd18e128fd201d99a7e
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-03-24 14:35:45 +0300

    Add files via upload

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -3,14 +3,13 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
-def print_hello(name):
-    return f'Hello {name}'
-your_name = 'Natalia'    
+def print_hello():
+    return 'Hello world from first Airflow DAG!'
 
 dag = DAG('my_first_dag', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello,op_kwargs={'name': your_name} dag=dag)
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
 hello_operator
