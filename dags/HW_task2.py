b2fa661386976c83fed2a93d1eaf5dffe6fdea53
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-03-25 20:27:01 +0300

    Create HW_task2.py

---
+++ dags/HW_task2.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import random
+
+def generate_and_square_number():
+    random_number = random.randint(1, 100)
+    squared_number = random_number ** 2
+    print(f"Random number: {random_number}, Squared number: {squared_number}")
+
+dag = DAG(
+    'generate_and_square_number_dag',
+    description='Generate and square a random number DAG',
+    schedule_interval='0 12 * * *',
+    start_date=datetime(2023, 1, 1),
+    catchup=False
+)
+
+generate_random_square = PythonOperator(
+    task_id='generate_random_square',
+    python_callable=generate_and_square_number,
+    dag=dag
+)
