3fb41334ba32240f20fdead1d3e14f42bf5baab4
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-03-30 19:52:47 +0300

    Update description_weather.py

--- dags/description_weather.py
+++ dags/description_weather.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.bash operator import BashOperator 
+from airflow.operators.bash_operator import BashOperator 
 from airflow.operators.python_operator import PythonOperator 
 from airflow.operators.http_operator import SimpleHttpOperator 
 import random 
