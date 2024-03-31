19b1ec048090c4ee4f0170ffaf63ef2c92deae58
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-03-31 11:20:14 +0300

    Update task3.py

--- dags/task3.py
+++ dags/task3.py
@@ -1,3 +1,21 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators import BashOperator,PythonOperator
+def print_hello ():
+    return 'Hello world from Airflow DAG!'
+def skipp():
+    return 99
+
+dag = DAG( 'hello_world' , description= 'Hello World DAG' ,
+schedule_interval= '0 12 * * *' ,
+start_date=datetime( 2023 , 1 , 1
+), catchup= False )
+
+hello_operator = PythonOperator(task_id= 'hello_task' , python_callable=print_hello, dag=dag)
+skipp_operator = PythonOperator(task_id= 'skip_task' , python_callable=skipp, dag=dag)
+hello_file_operator = BashOperator(task_id= 'hello_file_task' , bash_command='python/home/airflow/airflow/dags/scripts/file1.py', dag=dag)
+
+
 task_http_sensor_check = HttpSensor(
 task_id="http_sensor_check",
 http_conn_id="http_default",
@@ -7,4 +25,6 @@ response_check=lambda response: "httpbin" in response.text,
 poke_interval=5,
 dag=dag,
 )
+
+
 hello_operator >> skipp_operator >> hello_file_operator >> task_http_sensor_check
