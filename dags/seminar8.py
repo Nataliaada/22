116ffdff23f9f13257554a7c3b2bb79a198e5b27
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-03-31 10:58:56 +0300

    Update seminar8.py

--- dags/seminar8.py
+++ dags/seminar8.py
@@ -5,47 +5,60 @@ import pendulum
 from airflow.decorators import dag, task
 from airflow.providers.telegram.operators.telegram import TelegramOperator
 
-os.environ["no_proxy"] = "*"
+os.environ["no_proxy"]="*"
 
 @dag(
-    dag_id="weather-telegram",
+    dag_id="wether-tlegram",
     schedule="@once",
     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
     catchup=False,
-    dagrun_timeout=timedelta(minutes=60),
+    dagrun_timeout=datetime.timedelta(minutes=60),
 )
-def WeatherETL():
+
+def WetherETL():
 
     send_message_telegram_task = TelegramOperator(
         task_id='send_message_telegram',
-        telegram_conn_id='tg_main',
+        telegram_conn_id='telegram_default',
         token= '7191996568:AAHo61BtPMh1U5Ldyv8DxTGv2KOTLdaH9tU',
         chat_id='1211274716',
-        text='Weather in Moscow \nYandex: ' + "{{ ti.xcom_pull(task_ids=['yandex_weather'], key='weather')[0]}}" + " degrees" +
-             "\nOpen weather: " + "{{ ti.xcom_pull(task_ids=['open_weather'], key='weather')[0]}}" + " degrees",
+        text='Wether in Moscow \nYandex: ' + "{{ ti.xcom_pull(task_ids=['yandex_wether'],key='wether')[0]}}" + " degrees" +
+        "\nOpen wether: " + "{{ ti.xcom_pull(task_ids=['open_wether'],key='open_wether')[0]}}" + " degrees",
     )
 
-    @task(task_id='yandex_weather')
-    def get_yandex_weather(**kwargs):
+    @task(task_id='yandex_wether')
+    def get_yandex_wether(**kwargs):
         ti = kwargs['ti']
         url = "https://api.weather.yandex.ru/v2/informers/?lat=55.75396&lon=37.620393"
-        payload = {}
+
+        payload={}
         headers = {
-            'X-Yandex-API-Key': '33f45b91-bcd4-46e4-adc2-33cfdbbdd88e'
+        'X-Yandex-API-Key': '33f45b91-bcd4-46e4-adc2-33cfdbbdd88e'
         }
         response = requests.request("GET", url, headers=headers, data=payload)
-        ti.xcom_push(key='weather', value=response.json()['fact']['temp'])
-
-    @task(task_id='open_weather')
-    def get_open_weather(**kwargs):
+        print("test")
+        a=response.json()['fact']['temp']
+        print(a)
+        ti.xcom_push(key='wether', value=response.json()['fact']['temp'])
+#        return str(a)
+    @task(task_id='open_wether')
+    def get_open_wether(**kwargs):
         ti = kwargs['ti']
-        url = ("https://api.openweathermap.org/data/2.5/weather?lat=55.749013596652574&lon=37.61622153253021&"
-               "appid=2cd78e55c423fc81cebc1487134a6300")
-        payload = {}
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=55.749013596652574&lon=37.61622153253021&appid=2cd78e55c423fc81cebc1487134a6300"
+
+        payload={}
         headers = {}
+
         response = requests.request("GET", url, headers=headers, data=payload)
-        ti.xcom_push(key='weather', value=round(float(response.json()['main']['temp']) - 273.15, 2))
+        print("test")
+        a=round(float(response.json()['main']['temp']) - 273.15, 2)
+        print(a)
+        ti.xcom_push(key='open_wether', value=round(float(response.json()['main']['temp']) - 273.15, 2))
+#        return str(a)
+    @task(task_id='python_wether')
+    def get_wether(**kwargs):
+        print("Yandex "+str(kwargs['ti'].xcom_pull(task_ids=['yandex_wether'],key='wether')[0])+" Open "+str(kwargs['ti'].xcom_pull(task_ids=['open_wether'],key='open_wether')[0]))
+    
+    get_yandex_wether() >> get_open_wether() >> get_wether()>> send_message_telegram_task
 
-    get_yandex_weather() >> get_open_weather() >> send_message_telegram_task
-
-dag = WeatherETL()
+dag = WetherETL()
