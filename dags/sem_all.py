4df0fce29a7fc6f2b36d4c05b2e4844e9aba4c48
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-03-31 21:53:07 +0300

    Update sem_all.py

--- dags/sem_all.py
+++ dags/sem_all.py
@@ -12,34 +12,34 @@ start_date=datetime( 2021,4,1),
 catchup= False )
 
 def get_weather_data(ti = None):
-url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cde25583263075ff71ffa3961bd239d9"
-response = requests.request("GET", url)
+  url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cde25583263075ff71ffa3961bd239d9"
+  response = requests.request("GET", url)
 
-temperature_openweather = json.loads(response.text)['main']['temp'] - 273
-ti.xcom_push(key="temperature_openweather", value=temperature_openweather)
+  temperature_openweather = json.loads(response.text)['main']['temp'] - 273
+  ti.xcom_push(key="temperature_openweather", value=temperature_openweather)
 
-headers = {
-'X-Yandex-Weather-Key': "5366727e-2841-4d55-92d9-281216c4493d"
-}
+  headers = {
+  'X-Yandex-Weather-Key': "5366727e-2841-4d55-92d9-281216c4493d"
+  }
 
-response = requests.get('https://api.weather.yandex.ru/v2/forecast?lat=51.5085&lon=0.12574', headers=headers)
+  response = requests.get('https://api.weather.yandex.ru/v2/forecast?lat=51.5085&lon=0.12574', headers=headers)
 
-temperature_yandex = response.json()['fact']['temp']
+  temperature_yandex = response.json()['fact']['temp']
 
-ti.xcom_push(key="temperature_yandex", value=temperature_yandex)
+  ti.xcom_push(key="temperature_yandex", value=temperature_yandex)
 
 
 get_weather = PythonOperator(task_id = "get_weather_data",python_callable=get_weather_data,dag = sem8dag)
 
 send_tg_message = TelegramOperator(
-task_id='send_telega',
-token="7191996568:AAHo61BtPMh1U5Ldyv8DxTGv2KOTLdaH9tU",
-chat_id=1211274716,
-text='Температура в Лондоне по версии Openweather: \
-{{ ti.xcom_pull(key="temperature_openweather", task_ids="get_weather_data") }}\n \
-Температура в Лондоне по версии Yandex: \
-{{ ti.xcom_pull(key="temperature_yandex", task_ids="get_weather_data") }}',
-dag=sem8dag
-)
+  task_id='send_telega',
+  token="7191996568:AAHo61BtPMh1U5Ldyv8DxTGv2KOTLdaH9tU",
+  chat_id=1211274716,
+  text='Температура в Лондоне по версии Openweather: \
+  {{ ti.xcom_pull(key="temperature_openweather", task_ids="get_weather_data") }}\n \
+  Температура в Лондоне по версии Yandex: \
+  {{ ti.xcom_pull(key="temperature_yandex", task_ids="get_weather_data") }}',
+  dag=sem8dag
+  )
 
 get_weather >> send_tg_message
