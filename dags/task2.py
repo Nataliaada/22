371b9b79945894f92fcc8614c7575fc6e2c73b80
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-04-01 23:10:27 +0300

    Update task2.py

--- dags/task2.py
+++ dags/task2.py
@@ -1,16 +1,29 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators import BashOperator,PythonOperator
+from airflow.operators.bash import BashOperator
+
+
+dag = DAG( 'hello_world' , description= 'Hello World DAG' ,
+schedule_interval= '0 12 * * *' ,
+start_date=datetime( 2023 , 1 , 1
+), catchup= False )
+
+hello_operator = BashOperator(task_id= 'hello_task' , bash_command='echo Hello from Airflow', dag=dag)
+skipp_operator = BashOperator(task_id= 'skip_task' , bash_command='exit 99', dag=dag)
+hello_file_operator = BashOperator(task_id= 'hello_file_task' ,
+bash_command='./home/airflow/airflow/dags/scripts/file1.sh', dag=dag)
+hello_operator >> skipp_operator >> hello_file_operator
+
 def print_hello ():
     return 'Hello world from Airflow DAG!'
 def skipp():
     return 99
-dag = DAG( 'hello_world1' , description= 'Hello World DAG' ,
+dag2 = DAG( 'hello_world2' , description= 'Hello World DAG' ,
 schedule_interval= '0 12 * * *' ,
 start_date=datetime( 2023 , 1 , 1
 ), catchup= False 
 )
-hello_operator = PythonOperator(task_id= 'hello_task' , python_callable=print_hello, dag=dag)
-skipp_operator = PythonOperator(task_id= 'skip_task' , python_callable=skipp, dag=dag)
-hello_file_operator = BashOperator(task_id= 'hello_file_task' , bash_command='python/home/airflow/airflow/dags/scripts/file1.py', dag=dag)
-hello_operator >> skipp_operator >> hello_file_operator
+hello_operator2 = PythonOperator(task_id= 'hello_task2' , python_callable=print_hello, dag=dag2)
+skipp_operator2 = PythonOperator(task_id= 'skip_task2' , python_callable=skipp, dag=dag2)
+hello_file_operator2 = BashOperator(task_id= 'hello_file_task2' , bash_command='/home/airflow/airflow/dags/scripts/file1.py', dag=dag2)
+hello_operator2 >> skipp_operator2 >> hello_file_operator2
