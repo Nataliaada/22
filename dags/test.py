562dc81ff769440fd0a6499a08d68c462a6552f3
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-04-02 22:35:41 +0300

    Update test.py

--- dags/test.py
+++ dags/test.py
@@ -2,100 +2,56 @@ from datetime import datetime
 import pandas as pd
 import os
 from airflow.models import DAG
+from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 4, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1
-}
-
-dag = DAG(
-    'test',
-    default_args=default_args,
-    description='DAG for processing and loading data',
-    schedule_interval=None
-)
 
 def get_data(file_name):
     return pd.read_csv(file_name)
 
 def transform_data(**kwargs):
-    booking = kwargs['ti'].xcom_pull(task_ids='get_booking')
-    client = kwargs['ti'].xcom_pull(task_ids='get_client')
-    hotel = kwargs['ti'].xcom_pull(task_ids='get_hotel')
+    # Ваша функция transform_data без изменений
 
-    # Transformation steps
-    # Merge all tables
-    data = booking.merge(client, on='client_id').merge(hotel, on='hotel_id')
-    
-    # Data cleaning and transformation
-    data['booking_date'] = pd.to_datetime(data['booking_date'], format='%Y-%m-%d')  # Assuming booking date format is YYYY-MM-DD
-    data['currency'] = 'USD'  # Convert all currencies to USD
-    
-    relevant_columns = ['booking_date', 'client_id', 'name_client', 'age', 'type', 'hotel_id', 'name_hotel', 'room_type', 'booking_cost', 'currency']
-    data = data[relevant_columns]
+dag = DAG('homework8',
+          description='DAG for processing and loading data',
+          schedule_interval=None,
+          start_date=datetime(2024, 3, 26))
 
-    # Saving transformed data to a new CSV file
-    output_file = '/opt/airflow/dags/data_transformed.csv'
-    data.to_csv(output_file, index=False)
+get_booking = PythonOperator(task_id='get_booking', python_callable=get_data,
+                            op_args=['/opt/airflow/dags/booking.csv'], dag=dag)
+get_client = PythonOperator(task_id='get_client', python_callable=get_data,
+                           op_args=['/opt/airflow/dags/client.csv'], dag=dag)
+get_hotel = PythonOperator(task_id='get_hotel', python_callable=get_data,
+                          op_args=['/opt/airflow/dags/hotel.csv'], dag=dag)
 
-    return output_file
-def load_to_postgres(**kwargs):
-    sql_query = """
-    COPY data FROM '/opt/airflow/dags/data_transformed.csv' WITH CSV HEADER;
-    """
-    task = PostgresOperator(
-        task_id='load_to_postgres',
-        sql=sql_query,
-        postgres_conn_id='postgres_conn',
-        autocommit=True,
-        dag=dag
-    )
-    return task.execute(context=kwargs)
+transform_data_task = PythonOperator(task_id='transform_data_task',
+                                     python_callable=transform_data, dag=dag)
 
+create_table_postgres = PostgresOperator(task_id="create_data_table",
+                                         sql="""CREATE TABLE IF NOT EXISTS data(
+             booking_date DATE,
+             client_id INT,
+             name_client VARCHAR(50),
+             age INT,
+             type VARCHAR(20),
+             hotel_id INT,
+             name_hotel VARCHAR(50),
+             room_type VARCHAR(20),
+             booking_cost FLOAT,
+             currency VARCHAR(5));""",
+                                         postgres_conn_id='pg_conn',
+                                         database='airflow')
 
-# Task to get data from CSV files
-get_booking = PythonOperator(
-    task_id='get_booking',
-    python_callable=get_data,
-    op_args=['/opt/airflow/dags/booking.csv'],
-    dag=dag
-)
-
-get_client = PythonOperator(
-    task_id='get_client',
-    python_callable=get_data,
-    op_args=['/opt/airflow/dags/client.csv'],
-    dag=dag
-)
-
-get_hotel = PythonOperator(
-    task_id='get_hotel',
-    python_callable=get_data,
-    op_args=['/opt/airflow/dags/hotel.csv'],
-    dag=dag
-)
-
-transform_data_task = PythonOperator(
-    task_id='transform_data_task',
-    python_callable=transform_data,
-    provide_context=True,
-    dag=dag
-)
-
-load_to_postgres_task = PythonOperator(
-    task_id='load_to_postgres',
-    python_callable=load_to_postgres,
-    provide_context=True,
-    dag=dag
-)
+load_to_postgres_db = PostgresOperator(task_id='load_to_postgres_db',
+                                       postgres_conn_id='pg_conn',
+                                       sql="""COPY data FROM '/opt/airflow/dags/data.csv' WITH CSV HEADER;""",
+                                       dag=dag)
 
 get_booking >> transform_data_task
 get_client >> transform_data_task
 get_hotel >> transform_data_task
-transform_data_task >> load_to_postgres_task
+transform_data_task >> create_table_postgres >> load_to_postgres_db
+
+globals()['dag'] = dag
+
